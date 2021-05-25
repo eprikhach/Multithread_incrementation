1b2fd179b76456ea07c4f60e53f3bcb9439676ba
Eugene Prikhach <eugeneprikhach@icloud.com>
2021-05-25 19:11:03 +0300

    Changing implementation of the task
    Treads pool was used.
    The shared resource is explicitly transferred to the consumer.

--- multithreading.py
+++ multithreading.py
@@ -4,11 +4,8 @@ To solve this task we need:
 1. Lock function, which implement incrementation
 """
 
-# from incr import incrementation
-# print(incrementation(5, 1_000_000))
-# Second solution, which use c++ module(atomic vars + multithreading).
-
-from threading import Thread, Lock
+from concurrent.futures.thread import ThreadPoolExecutor
+from threading import Lock
 from loguru import logger
 
 # Removing basic logging handler, which is used for debugging.
@@ -20,33 +17,41 @@ logger.add('logs.log', level='INFO',
            rotation='1 MB', compression='zip')
 
 
+from incr import incrementation
+print(incrementation(5, 1_000_000))
+# Second solution, which use c++ module(atomic vars + multithreading).
+
+
+class Counter:
+
+    def __init__(self):
+        self.a = 0
+
+
+@logger.catch()
+def function(arg: int, counter: Counter, lock: Lock):
+    """Multithreading incrementation
+
+    :param lock: lock object
+    :param counter:
+    :param arg: number of incrementation
+    :return: None
+    """
+
+    for _ in range(arg):
+        lock.acquire()
+        counter.a += 1
+        lock.release()
+
+
 def main():
-    a = 0
     lock = Lock()
+    counter = Counter()
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for _ in range(5):
+            executor.submit(function(1000000, counter, lock, ))
 
-    @logger.catch()
-    def function(arg):
-        """Multithreading incrementation
-
-        :param arg: number of incrementation
-        :return: None
-        """
-
-        nonlocal a
-
-        for _ in range(arg):
-            lock.acquire()
-            a += 1
-            lock.release()
-
-    threads = []
-    for _ in range(5):
-        thread = Thread(target=function, args=(1_000_000,))
-        thread.start()
-        threads.append(thread)
-
-    [t.join() for t in threads]
-    print("----------------------", a)  # ???
+    print("----------------------", counter.a)  # ???
 
 
 if __name__ == "__main__":
